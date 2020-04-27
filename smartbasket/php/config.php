41f7053f2509f43ddbaa7f83db85985ea5d782aa
Steaq <gameslps6@gmail.com>
2020-04-27 04:24:31 +0300

    1

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -1,17 +1,17 @@
 <?php
     require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
 
-		// *** SMTP *** //
+		
 
-		 // require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
-		 // const HOST = '';
-		 // const LOGIN = '';
-		 // const PASS = '';
-		 // const PORT = '';
+		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		  const HOST = 'smtp.gmail.com';
+		  const LOGIN = 'gameslps6@gmail.com';
+		  const PASS = '17092002sss';
+		  const PORT = '465';
 
-		// *** /SMTP *** //
+		
    
-    const SENDER = 'sender@yandex.ru';
+    const SENDER = 'gameslps6@gmail.com';
     const CATCHER = 'vinzenzodecor@gmail.com';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
