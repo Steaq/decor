8810fc1e54a3a1e776db27e20f678e808b76b156
Steaq <gameslps6@gmail.com>
2020-05-08 02:14:02 +0300

    demo

---
+++ demo/php/config.php
@@ -0,0 +1,18 @@
+<?php
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/phpmailer.php');
+
+		// *** SMTP *** //
+
+		 // require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
+		 // const HOST = '';
+		 // const LOGIN = '';
+		 // const PASS = '';
+		 // const PORT = '';
+
+		// *** /SMTP *** //
+   
+    const SENDER = 'zzzelen01@mair.ru';
+    const CATCHER = 'kokag40395@zaelmo.com';
+    const SUBJECT = 'Заявка с сайта';
+    const CHARSET = 'UTF-8';
+    
