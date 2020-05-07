bcbdea808a790658574c0c8665705ef870559193
Steaq <gameslps6@gmail.com>
2020-05-08 01:07:55 +0300

    kekeksese

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -5,13 +5,13 @@
 
 		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
 		  const HOST = 'smtp.gmail.com';
-		  const LOGIN = 'gameslps6@gmail.com';
-		  const PASS = '17092002sss';
+		  const LOGIN = 'zzzelen01@mail.ru';
+		  const PASS = 'Ss123!!?';
 		  const PORT = '465';
 
 		
    
-    const SENDER = 'gameslps6@gmail.com';
+    const SENDER = 'zzzelen01@mail.ru';
     const CATCHER = 'vinzenzodecor@gmail.com';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
