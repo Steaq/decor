ec4a1c4f94e51063bd843ebf64be5d1c7961699d
Steaq <gameslps6@gmail.com>
2020-05-09 02:39:45 +0300

    porshe2512

--- smartbasket/php/config.php
+++ smartbasket/php/config.php
@@ -4,15 +4,15 @@
 		// *** SMTP *** //
 
 		  require_once($_SERVER['DOCUMENT_ROOT'] . '/smartbasket/php/phpmailer/smtp.php');
-		  const HOST = 'smtp.yandex.ru';
-		  const LOGIN = 'doris-alex@yandex.ua';
+		  const HOST = 'smtp.mail.ru';
+		  const LOGIN = 'zzzelen01@mail.ru';
 		  const PASS = 'Ss123!!?';
-		  const PORT = '587';
+		  const PORT = '465';
 
 		// *** /SMTP *** //
    
-    const SENDER = 'doris-alex@yandex.ua';
-    const CATCHER = 'kokag40395@zaelmo.com';
+    const SENDER = 'zzzelen01@mail.ru';
+    const CATCHER = 'vinzenzodecor@gmail.com';
     const SUBJECT = 'Заявка с сайта';
     const CHARSET = 'UTF-8';
     
