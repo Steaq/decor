b47ddd9f1afd4f1431fb71f8100a267fa5b788a5
Steaq <gameslps6@gmail.com>
2020-08-23 18:05:01 +0300

    vodep

--- static/js/games.js
+++ static/js/games.js
@@ -226,7 +226,21 @@ $backTop.on("click", () => {
 
 
 
+$(document).ready(function() {
 
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+        
+    });
+
+
+
+});
 
 
 
