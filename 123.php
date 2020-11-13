6d4a204badd34324aa03dae78071eff9542e74b2
Steaq <gameslps6@gmail.com>
2020-11-13 14:19:46 +0200

    php

---
+++ 123.php
@@ -0,0 +1,26 @@
+<?php
+/*$a1 = 10;
+$a2 = 5;
+$a3 = $a1 * $a2;
+echo $a3.', ';
+
+if ($a3 == 50) { 
+
+	echo "50"; 
+} else if ($a3 > 50) {
+	echo "0";
+} else {
+
+ echo "100";	
+}
+
+$q1 = true;
+$q2 = false;
+*/
+
+
+$white = rand(1, 5);
+if ($white == 2 or $white == 5) {
+	echo 'У меня всё получилось'; 
+}
+?>
