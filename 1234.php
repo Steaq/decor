5de118ab4be07aafad196dbd7baa1b2b5c19d381
Steaq <gameslps6@gmail.com>
2021-01-13 16:29:00 +0200

    4213a

--- 1234.php
+++ 1234.php
@@ -3,8 +3,9 @@ session_start();
 
 $a1 = rand(1, 9);
 $a2 = rand(1, 9);
-$a3 = $a1 * $a2;
+
 if (isset($_POST["answer"])) {
+	$a3 = $_POST["c1"] * $_POST["c2"];
 if ($a3 == $_POST["answer"]) {
 	echo 'Правильный ответ';
 } else {
@@ -19,6 +20,8 @@ if ($a3 == $_POST["answer"]) {
 <form action="" method="POST">
 	<?=$a1;?> * <?=$a2;?> = 
 	<input type="text" name="answer">
+	<input type="hidden" name="c1" value="<?=$a1;?>">
+	<input type="hidden" name="c2" value="<?=$a2;?>">
 	<button type="submit" name="">Отправить</button>
 	
 
